b041b578b7a7c00e1247ed531e8e45e70d28ebbe
vishakhamangtani <vishakhamangtani23@gmail.com>
2024-01-18 23:34:12 +0530

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UsersModule } from './users/users.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { UsersModule } from './users/users.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     NgbModule,
     UsersModule
   ],
